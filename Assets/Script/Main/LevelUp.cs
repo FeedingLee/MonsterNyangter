1573403ed167552a2a2d6d134eb619c1dde97d84
lww1398 <lww1398@gmail.com>
2024-12-27 22:00:37 +0900

    [12.27-22:00] fix : 레벨업 선택지 조건 변경 - 특정 레벨에는 무기 선택지만 등장하도록 변경함

--- Assets/Script/Main/LevelUp.cs
+++ Assets/Script/Main/LevelUp.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelUp : MonoBehaviour
 {
     RectTransform rect;
     Item[] items;
+
+    public Object GM_Objcet;  // 게임매니저 오브젝트 가져오기
 
     public RectTransform Joy;
     public RectTransform Stick;
@@ -58,26 +61,43 @@ public class LevelUp : MonoBehaviour
 
         // 2. 그 중에서 랜덤 3개 아이템 활성화
         int[] ran = new int[3];
-        while (true)
+
+        // 3. 5의 배수 레벨에는 무기만 선택지에 나타나도록 
+        if (GM_Objcet.GetComponent<GameManager>().level % 5 == 0)
         {
-            ran[0] = Random.Range(0, items.Length);
-            ran[1] = Random.Range(0, items.Length);
-            ran[2] = Random.Range(0, items.Length);
+            while (true)
+            {
+                ran[0] = Random.Range(0, 5);
+                ran[1] = Random.Range(0, 5);
+                ran[2] = Random.Range(0, 5);
 
-            if (ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2])
-                break;
+                if (ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2])
+                    break;
+            }
         }
+        else
+        {
+            while (true)
+            {
+                ran[0] = Random.Range(0, items.Length);
+                ran[1] = Random.Range(0, items.Length);
+                ran[2] = Random.Range(0, items.Length);
 
+                if (ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2])
+                    break;
+            }
+        }
+        
         for (int index = 0; index < ran.Length; index++)
         {
             Item ranItem = items[ran[index]];
 
-            // 3. 만렙 아이템의 경우는 소비아이템으로 대체
+            // 4. 만렙 아이템의 경우는 소비아이템으로 대체
             if (ranItem.level == ranItem.data.damages.Length)
             {
                 // 랜덤아이템이 많다면 (4~7번까지) 강의 12 [39:42] 참고
                 // items[Random.Range(4, 7)].gameObject.SetActive(true);
-                items[7].gameObject.SetActive(true); // 7번 = 회복
+                items[8].gameObject.SetActive(true); // 8번 = 회복
             }
             else
             {
