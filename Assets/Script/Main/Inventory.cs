65dd2a9c28cf686a53e92babb7e2390db79d964d
lww1398 <lww1398@gmail.com>
2025-01-19 20:15:36 +0900

    [01.19-20:14] feat : 만렙시 아이콘 변경 - 아이템 만렙시 인벤토리의 아이콘이 변경됨

--- Assets/Script/Main/Inventory.cs
+++ Assets/Script/Main/Inventory.cs
@@ -1,28 +1,41 @@
-using NUnit.Framework.Constraints;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static Cinemachine.DocumentationSortingAttribute;
 
 public class Inventory : MonoBehaviour
 {
     Text textLevel;                                         // 텍스트를 받아올 변수
-    public Object weapon_data;                              // 무기의 상태를 가지고있는, Weapon 오브젝트 받아오기
+    Image WeaponImg;                                        // 무기 이미지를 변경할 Image
+    public Sprite ChangeImg;                                // 바꿀 이미지 저장
+
+    public Object weapon_data;                              // 무기의 상태를 가지고 있는, Weapon 오브젝트 받아오기
     public int level;                                       // 레벨을 저장
 
     void Start()                                            // 최초 실행시
     {
-        Text[] text = GetComponentsInChildren<Text>();      // 자식오브젝트중 'Text'를 가진 컴포넌트를 찾아 Text의 배열에 넣는다
-        textLevel = text[0];                                // 가장 첫번째로 찾아진 Text는 Level을 담당하는 TextLevel이다
+        // 첫 번째 자식 오브젝트의 Image 컴포넌트를 가져오기
+        WeaponImg = transform.GetChild(0).GetComponent<Image>();
+
+        // 모든 자식 Text 컴포넌트를 배열로 가져오기
+        Text[] text = GetComponentsInChildren<Text>();
+        textLevel = text[0];                                // 첫 번째 Text 컴포넌트를 Level 텍스트로 할당
     }
-    void Update()                                           
-    {   
-        if (level != weapon_data.GetComponent<Item>().level) // 데이터 오브젝트가 레벨이 올랐는데, 현재 level과 다르다면 실행
-        { 
-            level = weapon_data.GetComponent<Item>().level;  // Level 변수에, 오브젝트로 연결한 무기의 현재 Level을 넣음
-            textLevel.text = "Lv." + (level);               
+
+    void Update()
+    {
+        // 데이터 오브젝트의 레벨이 현재 레벨과 다르면 업데이트
+        if (level != weapon_data.GetComponent<Item>().level)
+        {
+            level = weapon_data.GetComponent<Item>().level; // 현재 Level 업데이트
+            textLevel.text = "Lv." + level;                 // 레벨 텍스트 업데이트
+        }
+
+        // 만렙일 시 첫 번째 자식의 Image를 변경
+        if (level == 5)
+        {
+            WeaponImg.sprite = ChangeImg;                  // 만렙일 시 무기 아이콘 변경
         }
     }
 }
